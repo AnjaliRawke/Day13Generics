bba0507074a79a1ea806fcd578717d54a38121f4
AnjaliRawke <anjalirawke21@gmail.com>
2023-03-26 18:58:45 +0530

    Added:UC3 Find Maximum between 3 Using Generics

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -1,32 +1,17 @@
 package com.bridgelabz;
 
 public class Generics<T extends Comparable<T>> {
-//	public void FindMaxInt(Integer a, Integer b, Integer c) {
-//		int max = a;
-//		if ((b.compareTo(max)) > 0) {
-//			max = b;
-//		}
-//		if ((c.compareTo(max)) > 0) {
-//			max = c;
-//		}
-//		System.out.println("Maximum of " + a + "," + b + " and " + c + " is: " + max);
-//
-//	}
-
-//	public void FindMaxFloat(Float a, Float b, Float c){
-//		float max = a;
-//		if((b.compareTo(max))>0)
-//		{
-//			max=b;
-//		}
-//		if((c.compareTo(max))>0){
-//			max=c;
-//		}
-//		System.out.println("Maximum of "+a+ ","+b+" and "+c+ " is: "+max);
-//	}
-
-	public void FindMaxString(String a,String b,String c){
-		String max=a;
+	T x,y,z;
+	public Generics(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	public T FindMax(){
+		return Generics.FindMax(x,y,z);
+	}
+	public static <T extends Comparable<T>>T FindMax(T a, T b, T c){
+		T max=a;
 		if((b.compareTo(max))>0)
 		{
 			max=b;
@@ -35,32 +20,15 @@ public class Generics<T extends Comparable<T>> {
 			max=c;
 		}
 		System.out.println("Maximum of "+a+ ", "+b+" and "+c+ " is: "+max);
+		return max;
 	}
 
 	public static void main(String[] args) {
-		Generics genericsProgram = new Generics();
-//		Integer a = 40, b = 5, c = 3;
-//		// test case 1 :- Given Max Number at 1st Position return the Same Number
-//		genericsProgram.FindMaxInt(a, b, c);
-//		// test case 2 :- Given Max Number at 2nd Position return the Same Number
-//		genericsProgram.FindMaxInt(b, a, c);
-//		// test case 3 :- Given Max Number at 3rd Position return the Same Number
-//		genericsProgram.FindMaxInt(c, b, a);
-
-//		Float d = 12.5F ,e=5.5F,f=2.6F;
-//		// test case 1 :- Given Max Number at 1st Position return the Same Number
-//		genericsProgram.FindMaxFloat(d,e,f);
-//		// test case 2 :- Given Max Number at 2nd Position return the Same Number
-//		genericsProgram.FindMaxFloat(e,d,f);
-//		// test case 3 :- Given Max Number at 3rd Position return the Same Number
-//		genericsProgram.FindMaxFloat(d,f,d);
-
+		Integer a=22,b=4,c=6;
+		Float d = 12.3F ,e=5.9F,f=2.3F;
 		String g="Anjali",h="Rawke",i="Zain";
-		// test case 1 :- Given Max Number at 1st Position return the Same Number
-		genericsProgram.FindMaxString(i,h,g);
-		// test case 2 :- Given Max Number at 2nd Position return the Same Number
-		genericsProgram.FindMaxString(h,i,g);
-		// test case 3 :- Given Max Number at 3rd Position return the Same Number
-		genericsProgram.FindMaxString(g,h,i);
+		new Generics<Integer>(a,b,c).FindMax();
+		new Generics<Float>(d,e,f).FindMax();
+		new Generics<String>(g,h,i).FindMax();
 	}
 }
